b4f1afc23cb2ef802de71d11fc8fa114a03c3c0f
unknown <p.studzinski@soniqsoft.pl>
2022-12-19 19:49:44 +0100

    simple timer

---
+++ myapp/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<style>
+    body {
+        background-color: rgb(169, 197, 221);
+        color:rgb(105, 0, 243)
+    }
+</style>
+<body>
+    <h1>It's time to time</h1>
+    <button><a href='/timer'>Time it</a></button>
+</body>
---
+++ myapp/src/routes/timer/+page.svelte
@@ -0,0 +1,27 @@
+
+<script>
+	import { onMount } from "svelte";
+
+    let milisecond = 0;
+    let second = 0;
+    let minute = 0;
+    let hour = 0;
+	onMount(() => {
+		const interval = setInterval(() => {
+			milisecond = milisecond + 1;
+		}, 1);
+	});
+    if (milisecond === 1000) {second++; milisecond = 0;}
+    if (second === 60) {minute ++; second = 0;}
+    if (minute === 60) {hour ++; minute = 0;}
+    if (hour === 24) {hour = 0}
+</script>
+<style>
+
+</style><body>
+<p>you timed</p>
+<p>{milisecond} miliseconds</p>
+<p>{second} seconds</p>
+<p>{minute} minutes</p>
+<p>{hour} hours</p>
+<button><a href='./'>back</a></button></body>
