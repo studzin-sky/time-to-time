73343e8bb0b3ca288e9a01a0bbd8c26ab7d9c0ae
unknown <p.studzinski@soniqsoft.pl>
2022-12-20 11:42:31 +0100

    reactivity added

--- myapp/src/routes/timer/+page.svelte
+++ myapp/src/routes/timer/+page.svelte
@@ -1,26 +1,23 @@
 
-<script>
+<script lang='ts'>
 	import { onMount } from "svelte";
 
-    let milisecond = 0;
     let second = 0;
     let minute = 0;
     let hour = 0;
 	onMount(() => {
 		const interval = setInterval(() => {
-			milisecond = milisecond + 1;
-		}, 1);
+			second = second + 1;
+		}, 1000);
 	});
-    if (milisecond === 1000) {second++; milisecond = 0;}
-    if (second === 60) {minute ++; second = 0;}
-    if (minute === 60) {hour ++; minute = 0;}
-    if (hour === 24) {hour = 0}
+    $: if (second === 60) {minute ++; second = 0;}
+    $: if (minute === 60) {hour ++; minute = 0;}
+    $: if (hour === 24) {hour = 0}
 </script>
 <style>
 
 </style><body>
 <p>you timed</p>
-<p>{milisecond} miliseconds</p>
 <p>{second} seconds</p>
 <p>{minute} minutes</p>
 <p>{hour} hours</p>
